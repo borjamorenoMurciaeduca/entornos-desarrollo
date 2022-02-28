159d75cfbbc283cd97267d395dcdcd923878b6e6
borja.moreno <borja.moreno@um.es>
2022-02-28 22:31:01 +0100

    Últimos cambios práctica 4 entornos de desarrollo

--- DAM_ED04_TAR_R43_Actividad/DAM_ED04_Actividad/src/dam_ed04_actividad/CCuenta.java
+++ DAM_ED04_TAR_R43_Actividad/DAM_ED04_Actividad/src/dam_ed04_actividad/CCuenta.java
@@ -5,52 +5,113 @@
 
 package dam_ed04_actividad;
 
+/**
+ * 
+ * @author Borja Moreno
+ */
+
 
 public class CCuenta {
 
-
+    /**
+     * Nombre de la cuenta
+     */
     private String nombre;
+    
+    /**
+     * Número de cuenta
+     */
     private String cuenta;
+    
+    /**
+     * Saldo disponible en cuenta
+     */
     private double saldo;
+    
+    /**
+     * Tipo de interés de la cuenta
+     */
     private double tipoInterés;
 
-
+    /**
+     *
+     */
     public CCuenta()
     {
     }
 
+    /**
+     * 
+     * @param cuenta 
+     */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
+    /**
+     * 
+     * @param saldo 
+     */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
     
+    /**
+     * 
+     * @param nombre 
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
     
+    /**
+     * 
+     * @param tipoInterés 
+     */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
 
+    /**
+     * 
+     * @return cuenta
+     */
     public String getCuenta() {
         return cuenta;
     }
 
+    /**
+     * 
+     * @return saldo
+     */
     public double getSaldo() {
         return saldo;
     }
     
+    /**
+     * 
+     * @return nombre
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * 
+     * @return tipo de interés
+     */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
+    /**
+     * 
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo 
+     */
+    
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -58,30 +119,52 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     * 
+     * @param nom
+     * @deprecated En deuso, se usa el setter 
+     */
     public void asignarNombre(String nom)
     {
         setNombre(nom);
     }
 
+    /**
+     * 
+     * @return nombre
+     * @deprecated En desuso, se usa el getter
+     */
     public String obtenerNombre()
     {
         return getNombre();
     }
 
-
+    /**
+     * 
+     * @return estado
+     * @deprecated En desuso, se usa el getter
+     */
     public double estado()
     {
         return saldo;
     }
 
-
+    /**
+     * 
+     * @param cantidad
+     * @throws Exception 
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
-
+    /**
+     * 
+     * @param cantidad
+     * @throws Exception 
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -91,7 +174,11 @@ public class CCuenta {
         saldo = saldo - cantidad;
     }
 
-
+    /**
+     * 
+     * @return 
+     * @deprecated En desuso, se usa el getter
+     */
     public String obtenerCuenta()
     {
         return cuenta;
